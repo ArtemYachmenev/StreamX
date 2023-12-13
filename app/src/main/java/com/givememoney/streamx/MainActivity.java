bc3f267a6c441919215d6e14b905e430ef268efa
Artem <iachmeneow.a@yandex.ru>
2023-12-13 16:22:20 +0500

    hgj

--- app/src/main/java/com/givememoney/streamx/MainActivity.java
+++ app/src/main/java/com/givememoney/streamx/MainActivity.java
@@ -1,15 +1,61 @@
 package com.givememoney.streamx;
 
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.camera.core.CameraSelector;
+import androidx.camera.core.Preview;
+import androidx.camera.view.PreviewView;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity  {
 
+    //объявляем кнопки и прочее
+    ImageButton capture, toggleFlash, flipCamera;
+    private PreviewView previewView;
+    int cameraFacing= CameraSelector.LENS_FACING_BACK;
+    private final ActivityResultLauncher<String> activityResultLauncher=registerForActivityResult(
+            new ActivityResultContracts.RequestPermission(), new ActivityResultCallback<Boolean>() {
+                @Override
+                public void onActivityResult(Boolean result) {
+                    if (result){
+                     //   startCamera(cameraFacing);
+                    }
+                }
+            }
+    );
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        previewView=findViewById(R.id.cameraPreview);
+        capture=findViewById(R.id.capture);
+        toggleFlash=findViewById(R.id.toggleFlash);
+        flipCamera=findViewById(R.id.flipCamera);
+
+        if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CAMERA)!= PackageManager.PERMISSION_GRANTED){
+            activityResultLauncher.launch(Manifest.permission.CAMERA);
+        }
+        else{
+            //startCamera(cameraFacing);
+        }
+
+        flipCamera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (cameraFacing == CameraSelector.LENS_FACING_BACK){
+                   // cameraFacing=
+                }
+            }
+        });
     }
 
 }
